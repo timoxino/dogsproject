4604af561ddc263daca496d1882ae463f28db3dd
Tim Shieldsmith <timoxino@gmail.com>
2025-11-03 20:31:18 -0500

    Initial commit

---
+++ src/main/java/com/timoxino/java/spring/ai/dogsproject/DogsprojectApplication.java
@@ -0,0 +1,13 @@
+package com.timoxino.java.spring.ai.dogsproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DogsprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DogsprojectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/timoxino/java/spring/ai/dogsproject/DogsprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.timoxino.java.spring.ai.dogsproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DogsprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
